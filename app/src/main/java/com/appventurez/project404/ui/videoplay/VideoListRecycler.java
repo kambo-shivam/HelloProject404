9babcf025f23c2d92698dc20f16f0109236d4b2e
kamboSHiva <shivamkamboj49@gmail.com>
2019-07-04 19:47:31 +0530

    videoPlay

---
+++ app/src/main/java/com/appventurez/project404/ui/videoplay/VideoListRecycler.java
@@ -0,0 +1,59 @@
+package com.appventurez.project404.ui.videoplay;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.appventurez.project404.R;
+import com.appventurez.project404.databinding.RecycleVideoListBinding;
+import com.appventurez.project404.ui.roomdatabase.RecyclerClickListener;
+
+import java.util.ArrayList;
+
+public class VideoListRecycler extends RecyclerView.Adapter<VideoListRecycler.MyViewHolder> {
+    ArrayList<String> allVideoList;
+
+    private RecyclerClickListener clickListener;
+
+    public VideoListRecycler(Context context, ArrayList<String> allVideoList, RecyclerClickListener clickListener) {
+        this.allVideoList = allVideoList;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecycleVideoListBinding mBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.recycle_video_list, parent, false);
+        return new VideoListRecycler.MyViewHolder(mBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        String filename = allVideoList.get(position).substring(allVideoList.get(position).lastIndexOf("/") + 1);
+        holder.mBinding.videoName.setText(filename);
+        holder.mBinding.videoName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickListener.ItemClick(position, allVideoList.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return allVideoList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        RecycleVideoListBinding mBinding;
+
+        public MyViewHolder(RecycleVideoListBinding itemView) {
+            super(itemView.getRoot());
+            this.mBinding = itemView;
+        }
+    }
+}
--- app/src/main/java/com/appventurez/project404/ui/videoplay/VideoPlay.java
+++ app/src/main/java/com/appventurez/project404/ui/videoplay/VideoPlay.java
@@ -1,43 +1,100 @@
 package com.appventurez.project404.ui.videoplay;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.MediaController;
+import android.widget.Toast;
 
 import com.appventurez.project404.R;
 import com.appventurez.project404.databinding.ActivityVideoPlayBinding;
+import com.appventurez.project404.ui.roomdatabase.RecyclerClickListener;
+import com.iceteck.silicompressorr.SiliCompressor;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 
-public class VideoPlay extends AppCompatActivity implements View.OnClickListener {
+public class VideoPlay extends AppCompatActivity implements View.OnClickListener, RecyclerClickListener {
     ActivityVideoPlayBinding binding;
     final static int REQUEST = 222;
+    VideoListRecycler videoListRecycler;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_video_play);
         binding.openVideoCamera.setOnClickListener(this);
+        binding.playVideo.setOnClickListener(this);
+
+        getAllMedia();
+        setRecyclerView();
+    }
+
+    private void setRecyclerView() {
+        videoListRecycler = new VideoListRecycler(this, getAllMedia(), this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        binding.recyclerVideoList.setLayoutManager(layoutManager);
+        binding.recyclerVideoList.setAdapter(videoListRecycler);
+
+    }
+
+    public ArrayList<String> getAllMedia() {
+        HashSet<String> videoItemHashSet = new HashSet<>();
+
+        String[] projection = {MediaStore.Video.VideoColumns.DATA, MediaStore.Video.Media.DISPLAY_NAME};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+        try {
+            cursor.moveToFirst();
+            do {
+                videoItemHashSet.add((cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA))));
+            } while (cursor.moveToNext());
+
+            cursor.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> downloadedList = new ArrayList<>(videoItemHashSet);
+        return downloadedList;
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.open_video_camera:
-                File file = new File(Environment.getExternalStoragePublicDirectory("MyVideo").getAbsolutePath());
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+                File file = new File(Environment.getExternalStoragePublicDirectory("MyVideo" + currentDateTimeString).getAbsolutePath());
                 if (!file.exists())
                     file.mkdirs();
                 Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-                /*Uri fileUri = Uri.fromFile(file);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);*/
+                Uri fileUri = Uri.fromFile(file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
                 startActivityForResult(intent, REQUEST);
                 break;
+            case R.id.play_video:
+                if (uri != null) {
+                    binding.idVideoView.setVideoURI(uri);
+                    MediaController mediaController = new MediaController(this);
+                    binding.idVideoView.setMediaController(mediaController);
+                    mediaController.setAnchorView(binding.idVideoView);
+                    binding.idVideoView.start();
+                }
 
         }
     }
@@ -47,11 +104,55 @@ public class VideoPlay extends AppCompatActivity implements View.OnClickListener
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST && resultCode == RESULT_OK) {
             if (data != null) {
-                Uri uri = data.getData();
-                binding.idVideoView.setVideoURI(uri);
-
+                uri = data.getData();
 
             }
+        }
+
+    }
+
+    @Override
+    public void ItemClick(int pos, String string) {
+
+        if (string != null) {
+            File file = new File(string);
+            Log.d("SIZEOFFILEBEFORECOMP", String.valueOf((file.length() / (1024 * 1024))));
+
+            binding.idVideoView.setVideoURI(Uri.parse(string));
+            MediaController mediaController = new MediaController(getApplicationContext());
+            binding.idVideoView.setMediaController(mediaController);
+            mediaController.setAnchorView(binding.idVideoView);
+            binding.idVideoView.start();
+
+            new AsyncTask<String, Void, String>() {
+                @Override
+                protected String doInBackground(String... strings) {
+                    String fileVideoPath = null;
+                    try {
+                        File file1 = new File(string);
+                        fileVideoPath = SiliCompressor.with(getApplicationContext()).compressVideo(string, Environment.getExternalStoragePublicDirectory("you").getAbsolutePath());
+                        Log.d("SIZEOFFILE", String.valueOf((file1.length()) / (1024 * 1024)));
+
+                    } catch (URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                    return fileVideoPath;
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+                    Log.d("SIZEOFFILE", String.valueOf((s.length()) / (1024 * 1024)));
+                    binding.idVideoView.setVideoURI(Uri.parse(s));
+                    MediaController mediaController = new MediaController(getApplicationContext());
+                    binding.idVideoView.setMediaController(mediaController);
+                    mediaController.setAnchorView(binding.idVideoView);
+                    binding.idVideoView.start();
+
+                }
+            }.execute();
+
+
         }
 
     }
